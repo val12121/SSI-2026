56c5ad695f5bfa42ff831f44bb3c1eca8415869a
Camila Valentina Alonso Yepez <alu0101548999@ull.edu.es>
2026-03-26 18:05:29 +0000

    Lo he metido todo

--- S7/programa7/Program2.cs
+++ S7/programa7/Program2.cs
@@ -1,5 +1,7 @@
 namespace PracticaAES;
 using System;
+using System.Text;
+using System.Collections.Specialized;
 
 class CBC
 {
@@ -26,20 +28,80 @@ class CBC
     return state;
   }
 
+  public static byte[] Look (byte[] array, byte[] bloque_ant)
+  {
+    bool incompleto = false;
+    int auxiliar = 0;
+    
+    while (!incompleto && auxiliar < 15)
+    {
+      if (array[auxiliar].ToString() == "")
+      {
+        incompleto = true;
+      }
+      auxiliar++;
+    } 
+
+    Console.WriteLine($"{BitConverter.ToString(array).Replace("-","")}");
+
+    if (auxiliar < 16)
+    {
+      Console.WriteLine("El número introducido: ");
+      Console.WriteLine($"{BitConverter.ToString(array).Replace("-","")}");
+      Console.WriteLine("Vamos a proceder a su ampliación...");
+
+    }
+    byte[] resultado = Amplify(array, bloque_ant, auxiliar);
+    Console.WriteLine($"{BitConverter.ToString(resultado).Replace("-","")}");
+
+    return resultado;
+  }
+
+  public static byte[] Amplify (byte[] array, byte[] bloque_ant, int auxy)
+  {
+    int size_amp = 16 - auxy;
+    byte[] resultado = new byte [16];
+    
+    for (int i = 0; i < size_amp; i++)
+    {
+      resultado[i] = (byte)(array[i]);
+    }
+    for (int j = size_amp - 1; j < 16; j++)
+    {
+      resultado[j] = (byte)(bloque_ant[j]);
+    }
+    return resultado;
+  }
+
   public static void Main()
   {
     string original_S = "00000000000000000000000000000000";
     string key_S = "000102030405060708090a0b0c0d0e0f";
     string bloque_S = "00112233445566778899AABBCCDDEEFF";
-    string bloque_S2 = "00000000000000000000000000000000";
+
+    byte[] bloque_bloque = new byte [16];
+    bloque_bloque [0] = 0x00;
+    bloque_bloque [1] = 0x00;
+    bloque_bloque [2] = 0x00;
+    bloque_bloque [3] = 0x00;
+    bloque_bloque [4] = 0x00;
+    bloque_bloque [5] = 0x00;
+    bloque_bloque [6] = 0x00;
+    bloque_bloque [7] = 0x00;
+    bloque_bloque [8] = 0x00;
+    bloque_bloque [9] = 0x00;
+    bloque_bloque [10] = 0x00;
+    
 
     original_ = AESProgram.HexStringToByteArray(original_S);
     key_ = AESProgram.HexStringToByteArray(key_S);
     bloque_ = AESProgram.HexStringToByteArray(bloque_S);
     
     original_ = CBC_method(original_, key_, bloque_);
-    bloque_ = AESProgram.HexStringToByteArray(bloque_S2);
+    byte [] resultado1 = Look(bloque_bloque, bloque_);
+
+    bloque_ = bloque_bloque;
     byte[] resultado = CBC_method(original_, key_, bloque_);
+
   }
-  
 }
--- S7/programa7/obj/Debug/net10.0/programa7.AssemblyInfo.cs
+++ S7/programa7/obj/Debug/net10.0/programa7.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("programa7")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+883a0e8de7924848ab5e399121afef680497719b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a32d8e64158f8a30f7e610fc890fd2482f932ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("programa7")]
 [assembly: System.Reflection.AssemblyTitleAttribute("programa7")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
